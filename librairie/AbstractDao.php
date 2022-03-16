8c6d30035336e285cadbfbd6418c67c48ff831dd
Thibaut CHIRON <chiron.thibaut@sfr.fr>
2022-03-16 14:24:24 +0100

    Creation d'un AbstractDao

--- librairie/AbstractDao.php
+++ librairie/AbstractDao.php
@@ -1,39 +1,33 @@
 <?php
 
-class AbstractDao
+abstract class AbstractDao
 {
     protected static PDO $dbh;
-/**
- * Génères un objet PDO avec une connection à la base de données
- *
- * @return PDO Objet connecté à la base de données
- */
-public function __construct()
-{
-    if (!(AbstractDao::$dbh instanceof PDO)) {
-        $filepath = is_file('config' . DIRECTORY_SEPARATOR . 'config.ini') ?
-            'config' . DIRECTORY_SEPARATOR . 'config.ini' :
-            '..' . DIRECTORY_SEPARATOR . 'config' . DIRECTORY_SEPARATOR . 'config.ini';
-        $conf = parse_ini_file(
-            $filepath,
-            false,
-            INI_SCANNER_TYPED
-        );
-        $dbh = new PDO(
-            sprintf(
-                "%s:host=%s;dbname=%s;charset=%s",
-                $conf['driver'],
-                $conf['host'],
-                $conf['dbname'],
-                $conf['charset']
-            ),
-            $conf['username'],
-            $conf['password'], [
-                PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
-            ]
-        );
-    }
-}
 
-    return $dbh;
+    public function __construct()
+    {
+        if (!(isset(AbstractDao::$dbh) && AbstractDao::$dbh instanceof PDO)) {
+            $filepath = is_file('config' . DIRECTORY_SEPARATOR . 'config.ini') ?
+                'config' . DIRECTORY_SEPARATOR . 'config.ini' :
+                '..' . DIRECTORY_SEPARATOR . 'config' . DIRECTORY_SEPARATOR . 'config.ini';
+            $conf = parse_ini_file(
+                $filepath,
+                false,
+                INI_SCANNER_TYPED
+            );
+            AbstractDao::$dbh = new PDO(
+                sprintf(
+                    "%s:host=%s;dbname=%s;charset=%s",
+                    $conf['driver'],
+                    $conf['host'],
+                    $conf['dbname'],
+                    $conf['charset']
+                ),
+                $conf['username'],
+                $conf['password'], [
+                    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
+                ]
+            );
+        }
+    }
 }
