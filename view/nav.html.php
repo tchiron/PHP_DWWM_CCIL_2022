6190406402cf930ebb136f09d12b5a0f7206a404
Thibaut CHIRON <chiron.thibaut@sfr.fr>
2022-03-22 13:36:46 +0100

    Ajout de lien dans la navbar

--- view/nav.html.php
+++ view/nav.html.php
@@ -1,1 +1,7 @@
-<nav><!-- TODO --></nav>
+<nav>
+    <a href="/">Accueil</a>
+    <a href="/article/new">Nouvel article</a>
+    <a href="/signup">S'enregistrer</a>
+    <a href="/signin">Se connecter</a>
+    <a href="/signout">Se déconnecter</a>
+</nav>
