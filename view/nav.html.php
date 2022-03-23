d66267ffc72beb4254984c7b99bfa846d0561298
Thibaut CHIRON <chiron.thibaut@sfr.fr>
2022-03-23 12:15:29 +0100

    Modification de la navbar

--- view/nav.html.php
+++ view/nav.html.php
@@ -1,7 +1,10 @@
 <nav>
     <a href="/">Accueil</a>
+    <?php if (isset($_SESSION['user'])) : ?>
     <a href="/article/new">Nouvel article</a>
+    <a href="/signout">Se déconnecter</a>
+    <?php else : ?>
     <a href="/signup">S'enregistrer</a>
     <a href="/signin">Se connecter</a>
-    <a href="/signout">Se déconnecter</a>
+    <?php endif; ?>
 </nav>
