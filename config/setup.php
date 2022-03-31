862c97c44bd05eecbe1dbcece652cfdfa6d3b02b
Thibaut CHIRON <chiron.thibaut@sfr.fr>
2022-03-31 10:42:37 +0200

    Suppression des vues

--- config/setup.php
+++ config/setup.php
@@ -1,5 +1,4 @@
 <?php
 
 define('ROOT', realpath(__DIR__ . DIRECTORY_SEPARATOR . ".."));
-define('VIEW', ROOT . DIRECTORY_SEPARATOR . 'view');
 define('DATABASE_CONFIG_FILEPATH', implode(DIRECTORY_SEPARATOR, [ROOT, 'config', 'database.ini']));
