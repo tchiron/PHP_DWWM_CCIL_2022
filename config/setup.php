ace92d37292aa8da814a3cf8d1be31e97ea6990b
Thibaut CHIRON <chiron.thibaut@sfr.fr>
2022-03-31 10:45:47 +0200

    Suppression du fichier database.ini et de sa constante

--- config/setup.php
+++ config/setup.php
@@ -2,4 +2,3 @@
 
 define('ROOT', realpath(__DIR__ . DIRECTORY_SEPARATOR . ".."));
 define('VIEW', ROOT . DIRECTORY_SEPARATOR . 'view');
-define('DATABASE_CONFIG_FILEPATH', implode(DIRECTORY_SEPARATOR, [ROOT, 'config', 'database.ini']));
